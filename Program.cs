8f0cc90f3bd15daac5ff6060b47fea35067d6a8b
Anusha Mahadev <anusham7646@gmail.com>
2022-08-24 13:33:05 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Simple C# program to illustrate identifiers
+using System;
+  
+class GFG
+{
+
+    // Main Method
+    static public void Main()
+    {
+
+        // variable
+        int a = 10;
+        int b = 39;
+        int c;
+
+        // simple addition
+        c = a + b;
+        Console.WriteLine("The sum of two number is: {0}", c);
+    }
+}
